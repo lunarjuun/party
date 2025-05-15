e5adc2420f294ffccd64dff7b6b1614d80d06ffa
lunarjuun <157792207+lunarjuun@users.noreply.github.com>
2025-05-15 10:15:20 +0200

    fixed the loading of the index gif

--- årsoppgavepractice/static/script.js
+++ årsoppgavepractice/static/script.js
@@ -1,21 +1,16 @@
-const preloadImages = [new Image(), new Image()]
-preloadImages[0].src = img1;
-preloadImages[1].src = img2;
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadImages = [new Image(), new Image()];
+  preloadImages[0].src = img1;
+  preloadImages[1].src = img2;
 
-const img = document.getElementById('toggleImage')
-const img1 = '../static/media/dealer/har1.gif';
-const img2 = '../static/media/dealer/har2.gif';
-
-let showingFirst = true;
+  const img = document.getElementById('toggleImage');
+  let showingFirst = true;
 
   setInterval(() => {
-    if (showingFirst) {
-      img.src = img2;
-    } else {
-      img.src = img1;
-    }
+    img.src = showingFirst ? img2 : img1;
     showingFirst = !showingFirst;
   }, 1500);
+});
 
 function partyTime() {
     let sound = new Audio("static/media/confetti-pop.mp3"); // allows audio overlapping for spam
